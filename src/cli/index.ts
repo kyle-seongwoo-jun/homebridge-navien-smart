4fe15415ef2db2e60327d4ac39ba70fdb3d19c6b
Kyle Seongwoo Jun <njk230@gmail.com>
2024-02-14 16:07:02 +0900

    feat: Add CLI script

---
+++ src/cli/index.ts
@@ -0,0 +1,28 @@
+/* eslint-disable no-console */
+
+import { AuthException } from '../navien/exceptions';
+import { NavienAuth } from '../navien/navien.auth';
+
+if (process.argv.length < 4) {
+  console.error('Usage: navien <username> <password>');
+  process.exit(1);
+}
+
+async function main() {
+  const username = process.argv[2];
+  const password = process.argv[3];
+
+  const auth = new NavienAuth(console);
+
+  const response = await auth.login(username, password);
+  console.log('refreshToken:', response.refreshToken);
+  console.log('accountSeq:', response.userSeq);
+}
+
+main().catch((error) => {
+  if (error instanceof AuthException) {
+    console.error('Error:', error.message);
+    return;
+  }
+  console.error('Unknown error occurred. Please report this to the developer:', error);
+});
